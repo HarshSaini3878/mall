8b9451501256f97570ab0ae668b5b5109d455c7a
HarshSaini3878 <142294106+HarshSaini3878@users.noreply.github.com>
2025-07-01 09:20:18 +0530

    removed some routes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import navigationRoute from './routes/Navigation.js';
+
 import userRoute from './routes/user.routes.js';
 import { configDotenv } from 'dotenv';
 const app = express();
@@ -15,7 +15,7 @@ mongoose.connect('mongodb://localhost:27017/mall', {
 }).then(() => console.log('✅ MongoDB connected'))
   .catch(err => console.error('❌ MongoDB connection error:', err));
 
-app.use('/api', navigationRoute);
+
 app.use('/api/v1/user', userRoute);
 
 app.listen(5000, () => console.log('✅ Server running on port 5000'));
